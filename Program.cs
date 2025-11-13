236b37a89ff8637a1062ad525b5c321d206689d4
Defrunct <80599378+Defrunct@users.noreply.github.com>
2025-11-13 21:44:58 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace IntroductionToTeamCity
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, TeamCity!");
+        }
+    }
+}
